12e7b63ed0b9817445c3acc62b8aa61ea2fbd93e
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2023-12-31 15:06:53 -0600

    initial functional commit

---
+++ generate.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Generate a HTML file with what the user is working on. Open the file in a browser.
+"""
+
+import os
+from argparse import ArgumentParser, Namespace
+from time import localtime, strftime
+from webbrowser import open as open_html
+
+
+def parse_args() -> Namespace:
+    """Parse inputted arguments"""
+    parser = ArgumentParser()
+    parser.add_argument("input_string", nargs="+")
+    return parser.parse_args()
+
+
+def generate_html(input_string: str) -> str:
+    """
+    Open `template.html`. Fill in template variables with computed strings.'
+    """
+    with open("template.html", "r", encoding="utf-8") as file:
+        template = file.read()
+    return template.replace("{{ input_string }}", input_string)
+
+
+def main() -> None:
+    """
+    Entry point for What Am I Working On
+    """
+    input_string = " ".join(parse_args().input_string)
+    filename = strftime("%Y-%m-%d_%H%M%S.html", localtime())
+    filepath = f"html/{filename}"
+    with open(filepath, "w", encoding="utf-8") as file:
+        file.write(generate_html(input_string))
+    open_html(f"file://{os.path.realpath(filepath)}")
+
+
+if __name__ == "__main__":
+    main()
